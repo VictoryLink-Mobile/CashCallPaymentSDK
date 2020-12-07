ae6c52df3f7de380272b2697832321a4f286eec0
Ahmed M. Hassan <ahassan@victorylink.com>
2020-12-07 14:20:07 +0200

    Add CashCall payment SDK for simulator environment

---
+++ CashcallPaymentSDK.framework/Headers/CashcallPaymentSDK.h
@@ -0,0 +1,19 @@
+//
+//  CashcallPaymentSDK.h
+//  CashcallPaymentSDK
+//
+//  Created by cashcall on 9/3/20.
+//  Copyright © 2020 cashcall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CashcallPaymentSDK.
+FOUNDATION_EXPORT double CashcallPaymentSDKVersionNumber;
+
+//! Project version string for CashcallPaymentSDK.
+FOUNDATION_EXPORT const unsigned char CashcallPaymentSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CashcallPaymentSDK/PublicHeader.h>
+
+
